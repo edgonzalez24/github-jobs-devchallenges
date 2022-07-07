bdde80818b97dbffd228ed532df1f03a0397aaf1
Edwin <edgoca2498@gmail.com>
2022-07-07 08:53:29 -0600

    style: Update style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="w-full pt-2 pb-5 flex justify-center">
     <div class="font-montserrat text-gray-100 w-auto m-auto text-sm">
       created by&nbsp;
-      <a class="underline font-bold focus:outline-none transition duration-500 ease-in-out hover:text-gray-500" :href="url" target="_blanck">edgonzalez24</a>
+      <a class="underline font-bold focus:outline-none transition duration-500 ease-in-out hover:text-gray-500 cursor-pointer" :href="url" target="_blanck">edgonzalez24</a>
       &nbsp;- devChallenges.io
     </div>
   </div>
